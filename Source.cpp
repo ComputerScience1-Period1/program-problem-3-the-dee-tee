8c1cee4fc75cd37b07c087a30f49a41907f0a5fb
spicyboi <31770245+spicyboi@users.noreply.github.com>
2017-10-12 08:40:06 -0500

    Update Source.cpp
    Not entirely sure if this would run w/o errors but it's simplifying it.

--- Source.cpp
+++ Source.cpp
@@ -7,27 +7,19 @@ void main()
 	cout << "please enter a 3 digit number senior: ";
 	int original;
 	cin >> original;
-	cout << "\nthank you.\n";
+	cout << "\n thank you.\n";
 	int onesDigit = original - (original / 10 * 10);
 	int tensDigit = (original - (original / 100 * 100) - (onesDigit)) / 10;
 	int hundredsDigit = (original - (original / 1000 * 1000) - onesDigit - tensDigit) / 100;
+	if (onesDigit > tensDigit && tensDigit > hundredsDigit) {
+		cout << "Descending";
+	}
+	else if (hundredsDigit > tensDigit && tensDigit > hundredsDigit) {
+		cout << "Ascending";
+	}
+	else {
+		cout << "Neither ascending nor descending";
+	};
 
-	bool ascending = onesDigit != tensDigit && tensDigit != hundredsDigit;
-	bool descending = ascending;
-	if (onesDigit > tensDigit)
-		ascending = false;
-	else
-		descending = false;
-	if (tensDigit > hundredsDigit)
-		ascending = false;
-	else
-		descending = false;
-
-	if (ascending)
-		cout << "Ascending \n";
-	else if (descending)
-		cout << "Descending \n";
-	else
-		cout << "Neither";
-	_getch();
+	pause();
 }
